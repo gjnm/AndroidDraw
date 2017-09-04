30b0fc1857b66a5dd66e516f3c8b1a7c125be89f
gaojian <gaojian12@baidu.com>
2017-09-04 20:44:22 +0800

    change:添加动态的VectorDrawable

--- app/src/main/java/com/gjnm/androiddraw/VectorActivity.java
+++ app/src/main/java/com/gjnm/androiddraw/VectorActivity.java
@@ -1,13 +1,18 @@
 package com.gjnm.androiddraw;
 
+import android.graphics.drawable.Animatable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
 
 /**
  * Created by gaojian12 on 17/8/27.
  */
 
 public class VectorActivity extends AppCompatActivity {
+    public ImageView arrow;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,7 +21,10 @@ public class VectorActivity extends AppCompatActivity {
     }
 
     private void init() {
-
-
+        arrow = (ImageView) findViewById(R.id.arrow);
+        Drawable drawable = arrow.getDrawable();
+        if(drawable instanceof Animatable){
+            ((Animatable)drawable).start();
+        }
     }
 }
